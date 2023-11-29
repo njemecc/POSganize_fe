729e33ca5326bcde02df0eca1c4423b63c3a35b3
brani <branislannjemec@gmail.com>
2023-11-29 23:28:30 +0100

    implementing memberships feature

---
+++ src/features/memberships/MempbershipTable.jsx
@@ -0,0 +1,65 @@
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import { TableFooter } from "@mui/material";
+import Paper from "@mui/material/Paper";
+import { useGetMembershipByUserId } from "./useGetMembershipByUserId";
+
+//components
+import Spinner from "../../ui/Spinner";
+import Menus from "../../ui/Menus";
+import UserRow from "./UserRow";
+import Pagination from "../../ui/Pagination";
+
+export default function MembershipTable() {
+  const { memberships, loadingMemberships } = useGetMembershipByUserId();
+
+  console.log(memberships);
+
+  if (loadingMemberships) return <Spinner />;
+
+  return (
+    <>
+      <Menus>
+        <TableContainer component={Paper}>
+          <Table
+            sx={{ minWidth: 660, minHeight: 500 }}
+            aria-label="simple table"
+          >
+            <TableHead>
+              <TableRow>
+                <TableCell sx={{ fontSize: 15, fontWeight: 600 }}>
+                  First Name
+                </TableCell>
+                <TableCell sx={{ fontSize: 15, fontWeight: 600 }} align="left">
+                  Last Name
+                </TableCell>
+                <TableCell sx={{ fontSize: 15, fontWeight: 600 }} align="left">
+                  Email
+                </TableCell>
+                <TableCell sx={{ fontSize: 15, fontWeight: 600 }} align="left">
+                  Number
+                </TableCell>
+                <TableCell sx={{ fontSize: 15, fontWeight: 600 }} align="left">
+                  Active
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {users.map((user) => (
+                <UserRow key={user.id} user={user} />
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TableFooter sx={{ margin: "0 auto" }}>
+          <Pagination count={Number(numberOfUsers)} />
+        </TableFooter>
+      </Menus>
+    </>
+  );
+}
