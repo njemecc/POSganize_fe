a85c00a8979cb0b17d013c341ce0577927754e5d
brani <branislannjemec@gmail.com>
2023-12-05 12:57:17 +0100

    Add Link to signup form

---
+++ src/features/authentication/LoginForm.jsx
@@ -0,0 +1,96 @@
+import "./LoginForm.css";
+import { useState } from "react";
+import Button from "../../ui/Button";
+import Form from "../../ui/Form";
+import FormRowVertical from "../../ui/FormRowVertical";
+import Input from "../../ui/Input";
+import SpinnerMini from "../../ui/SpinnerMini";
+import { useLogin } from "./useLogin";
+import { TextField } from "@mui/material";
+import { Link } from "react-router-dom";
+function LoginForm() {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const { login, isLoading } = useLogin();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!email || !password) return;
+    login(
+      { email, password }
+      // {
+      //   onSettled: () => {
+      //     setEmail("");
+      //     setPassword("");
+      //   },
+      // }
+    );
+  }
+
+  //styles
+  const labelStyles = {
+    fontSize: "1.5rem",
+  };
+
+  const inputStyles = {
+    fontSize: "1.5rem",
+  };
+
+  return (
+    <div className="login-form-container">
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <TextField
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            required
+            id="outlined-basic"
+            label="Email"
+            variant="outlined"
+            InputProps={{
+              style: inputStyles,
+            }}
+            InputLabelProps={{
+              style: labelStyles,
+            }}
+            style={{ width: "100%" }}
+          />
+        </div>
+        <div className="form-group">
+          <TextField
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            required
+            id="outlined-basic"
+            label="Password"
+            variant="outlined"
+            InputProps={{
+              style: inputStyles,
+            }}
+            InputLabelProps={{
+              style: labelStyles,
+            }}
+            style={{ width: "100%" }}
+          />
+        </div>
+
+        <div className="button-wrapper">
+          <Button
+            type="submit"
+            className="custom-button"
+            size="large"
+            // disabled={isLoading}
+          >
+            {!isLoading ? "Log in" : <SpinnerMini />}{" "}
+          </Button>
+        </div>
+        <p className="signup-prompt">
+          Don't have an account? <Link to="/signup">Sign up</Link>
+        </p>
+      </form>
+    </div>
+  );
+}
+
+export default LoginForm;
