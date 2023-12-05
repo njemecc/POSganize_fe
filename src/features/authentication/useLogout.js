5c36a103c33651bf6c9a4d88921d17002ab41742
brani <branislannjemec@gmail.com>
2023-12-05 12:59:21 +0100

    create useLogout custom hook

---
+++ src/features/authentication/useLogout.js
@@ -0,0 +1,22 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+import { logout as logoutApi } from "../../services/apiAuth";
+import { toast } from "react-hot-toast";
+
+export function useLogout() {
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
+
+  const { mutate: logout, isLoading } = useMutation({
+    mutationFn: logoutApi,
+    onSuccess: () => {
+      queryClient.removeQueries();
+      navigate("/login");
+      localStorage.removeItem("user");
+      toast.succes("Successfully logged out ");
+    },
+    onError: (err) => console.error(err),
+  });
+
+  return { logout, isLoading };
+}
