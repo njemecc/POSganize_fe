8f1160135895faeb468a2f1a57e62821731bef58
brani <branislannjemec@gmail.com>
2023-12-05 13:04:23 +0100

    update styles

--- src/features/trainings/TrainingCard.jsx
+++ src/features/trainings/TrainingCard.jsx
@@ -4,6 +4,9 @@ import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { Button, CardActionArea, CardActions } from "@mui/material";
 
+//styles
+import "./TrainingCard.css";
+
 //icons
 import { HiPencil, HiTrash } from "react-icons/hi2";
 
@@ -18,10 +21,15 @@ import { useDeleteTraining } from "./useDeleteTraining";
 
 //router
 import { useNavigate } from "react-router-dom";
+import { useUser } from "../authentication/useUser";
+import { ADMIN } from "../../utils/roles";
 
 export default function TrainingCard({ training }) {
-  const { id, name, image, price, description } = training;
+  const { id, name, image, price } = training;
   const { deleteTraining, isDeleting } = useDeleteTraining();
+
+  //authorization
+  const { role } = useUser();
 
   const [showDeleting, setShowDeleting] = useState(false);
   const [showEdit, setShowEdit] = useState(false);
@@ -30,69 +38,21 @@ export default function TrainingCard({ training }) {
   const navigate = useNavigate();
 
   return (
-    <>
-      <Card sx={{ maxWidth: 345, marginTop: 10, width: "33%" }}>
-        <CardActionArea onClick={() => navigate(`/trainings/${id}`)}>
-          <CardMedia component="img" height="200" image={image} alt={name} />
-          <CardContent>
-            <Typography gutterBottom variant="h4" component="div">
-              {name}
-            </Typography>
-            {/* <Typography variant="body1" color="text.secondary">
+    <Card className="training-card">
+      <CardActionArea onClick={() => navigate(`/trainings/${id}`)}>
+        <CardMedia component="img" height="200" image={image} alt={name} />
+        <CardContent className="card-content">
+          <Typography gutterBottom variant="h4" component="div">
+            {name}
+          </Typography>
+          {/* <Typography variant="body1" color="text.secondary">
               {description
                 ? description
                 : "Kick box is a great sport for upgrading your fighting skills and boost your confidance!"}
             </Typography> */}
-            <Typography variant="h5">{price}$</Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button
-            onClick={() => {
-              setShowEdit(true);
-              setWhatModal(id);
-            }}
-            variant="contained"
-            sx={{ fontSize: "1.3rem" }}
-          >
-            <HiPencil />
-          </Button>
-          <Button
-            sx={{ fontSize: "1.3rem" }}
-            onClick={() => {
-              setShowDeleting(true);
-              setWhatModal(id);
-            }}
-            disabled={isDeleting}
-            variant="contained"
-            color="error"
-          >
-            <HiTrash />
-          </Button>
-        </CardActions>
-      </Card>
-      {showDeleting && whatModal === id ? (
-        <Modal onClose={() => setShowDeleting(false)}>
-          <ConfirmDelete
-            onConfirm={() => deleteTraining(id)}
-            disabled={isDeleting}
-            closeModal={() => setShowDeleting(false)}
-          />
-        </Modal>
-      ) : (
-        ""
-      )}
-      {showEdit && whatModal === id ? (
-        <Modal onClose={() => setShowEdit(false)}>
-          <CreateEditTrainingForm
-            onClose={() => setShowEdit(false)}
-            edit={true}
-            training={training}
-          />
-        </Modal>
-      ) : (
-        ""
-      )}
-    </>
+          <Typography variant="h5">{price}$</Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
   );
 }
