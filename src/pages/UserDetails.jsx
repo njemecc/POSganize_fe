8b4bf03886ee11441244dbb6855013dd5f13f0ca
brani <branislannjemec@gmail.com>
2023-12-06 12:49:43 +0100

    check if role is Admin and change Paragraph text

--- src/pages/UserDetails.jsx
+++ src/pages/UserDetails.jsx
@@ -1,14 +1,25 @@
 //components
+import { useUser } from "../features/authentication/useUser";
 import AddMembership from "../features/memberships/AddMembership";
 import MembershipTable from "../features/memberships/MempbershipTable";
 import UserDetailsHeader from "../features/users/userDetailsHeader";
 import Heading from "../ui/Heading";
+import Spinner from "../ui/Spinner";
+import { ADMIN } from "../utils/roles";
 
 const UserDetails = () => {
+  const { role, isLoading } = useUser();
+
+  isLoading && <Spinner />;
+
   return (
     <>
       <UserDetailsHeader />
-      <Heading as="h2">All User Memberships</Heading>
+
+      <Heading as="h2">
+        {" "}
+        {role === ADMIN ? "All User Memberships" : "Your membership history"}
+      </Heading>
       <MembershipTable />
       <AddMembership />
     </>
