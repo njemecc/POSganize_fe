2cd65fc7e73b15d26300318b7c6b5712bf23039d
brani <branislannjemec@gmail.com>
2023-12-20 17:15:58 +0100

    create useCreateNews custom hook

---
+++ src/features/news/useCreateNews.js
@@ -0,0 +1,28 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "react-hot-toast";
+
+import { createNews as createNewsApi } from "../../services/apiNews";
+
+export function useCreateNews() {
+  const queryClient = useQueryClient();
+
+  const {
+    mutate: createNews,
+    isLoading,
+    isCreating,
+  } = useMutation({
+    mutationFn: createNewsApi,
+    onSuccess: () => {
+      toast.success("News News successfully created");
+      queryClient.invalidateQueries({
+        queryKey: ["news"],
+      });
+    },
+    onError: (err) => {
+      toast.error(err.message);
+      console.log("error is", err);
+    },
+  });
+
+  return { createNews, isCreating };
+}
