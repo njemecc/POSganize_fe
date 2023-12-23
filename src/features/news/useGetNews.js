c5a9927388c4e63aab45ecffdeaf46bdeb6bd050
brani <branislannjemec@gmail.com>
2023-12-23 22:14:08 +0100

    add useGetNews custom hook

--- src/features/news/useGetNews.js
+++ src/features/news/useGetNews.js
@@ -2,15 +2,23 @@ import { useInfiniteQuery } from "@tanstack/react-query";
 import { getAllNews } from "../../services/apiNews";
 
 export function useGetNews() {
-  const { data, fetchNextPage, hasNextPage } = useInfiniteQuery({
+  const { data, fetchNextPage, hasNextPage, isLoading } = useInfiniteQuery({
     queryKey: ["news"],
     queryFn: getAllNews,
     getNextPageParam: (lastPage) => {
+      console.log(lastPage);
       if (lastPage.hasNext === false) {
-        return false;
+        console.log("E1");
+        return undefined;
       }
 
       return lastPage.pageNumber + 1;
     },
   });
+
+  const allNews = data?.pages.reduce((acc, page) => {
+    return [...acc, ...page.clubNews];
+  }, []);
+
+  return { data, allNews, fetchNextPage, hasNextPage, isLoading, hasNextPage };
 }
