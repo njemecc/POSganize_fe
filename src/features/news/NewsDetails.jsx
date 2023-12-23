ac425ed7e04e514e645dc566c3e8de0deecfbaa2
brani <branislannjemec@gmail.com>
2023-12-23 22:30:42 +0100

    implement UseGetNewsById hook

--- src/features/news/NewsDetails.jsx
+++ src/features/news/NewsDetails.jsx
@@ -1,21 +1,14 @@
 import React from "react";
+import Spinner from "../../ui/Spinner";
 import styles from "./NewsDetails.module.css";
+import { useGetNewsById } from "./useGetNewsById";
 
 const NewsDetails = () => {
-  const post = {
-    imageUrl:
-      "https://nova.rs/wp-content/uploads/2023/11/19/1700421167-Tan2023-11-1920112216_4-750x500.jpg",
-    title: "MMA championship",
-    text: `Mixed martial arts (MMA)[a] is a full-contact combat sport based on striking, grappling and ground fighting, incorporating techniques from various combat sports from around the world.[10] The first documented use of the term mixed martial arts was in a review of UFC 1 by television critic Howard Rosenberg in 1993.[11]
-  
-        During the early 20th century, various interstylistic contests took place throughout Japan and in the countries of the East Asia. At the same time, in Brazil, there was a phenomenon called vale tudo, which became known for unrestricted fights between various styles such as judo/Brazilian jiu-jitsu, catch wrestling/luta livre, Muay Thai, and capoeira. Another precursor to modern MMA was the 1976 Ali vs. Inoki exhibition bout (which ended in a draw after 15 rounds), fought between boxer Muhammad Ali and wrestler Antonio Inoki in Japan, where it later inspired the foundation of Shooto in 1985, Pancrase in 1993, and the Pride Fighting Championships in 1997.
-        
-        In the 1990s, the Gracie family brought their Brazilian jiu-jitsu style, first developed in Brazil from the 1920s, to the United States – which culminated in the founding of the Ultimate Fighting Championship (UFC) promotion company in 1993. The company held an event with almost no rules, mostly due to the influence of Art Davie and Rorion Gracie attempting to replicate vale tudo fights that existed in Brazil[12] and would later implement a different set of rules (example: eliminating kicking a grounded opponent), which differed from other leagues which were more in favour of realistic, street like fights.[13]"`,
-    createdAt: "2023-12-19",
-    author: "John Doe",
-  };
+  const { news, loadingNews } = useGetNewsById();
 
-  const { title, description, image, createdAt, author } = post;
+  if (loadingNews) return <Spinner />;
+
+  const { title, description, image, createdAt, author } = news;
 
   return (
     <div className={styles["blog-details"]}>
--- src/features/news/useGetNewsById.js
+++ src/features/news/useGetNewsById.js
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
 import { getNewsById } from "../../services/apiNews";
 
-export function useGetUser() {
+export function useGetNewsById() {
   const { newsId } = useParams();
 
   const { data: news, isLoading: loadingNews } = useQuery({
