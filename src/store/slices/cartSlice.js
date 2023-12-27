52ac5c326df0827d503e59515db04ec7a795fcb0
brani <branislannjemec@gmail.com>
2023-12-27 15:59:08 +0100

    create cartSlice

---
+++ src/store/slices/cartSlice.js
@@ -0,0 +1,43 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-hot-toast";
+
+const initialState = {
+  cartItems: [],
+  totalQuantity: 0,
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items?.find((item) => item.id === newItem.id);
+
+      state.totalQuantity++;
+
+      if (!existingItem) {
+        state.cartItems.push(newItem);
+      } else {
+        toast.error(`${newItem.name} alredy exists in your cart`);
+      }
+    },
+    removeFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.cartItems.find((item) => item.id === id);
+
+      if (existingItem) {
+        state.totalQuantity--;
+
+        state.cartItems = state.cartItems.filter((item) => item.id !== id);
+      }
+    },
+    clearCart(state) {
+      state.cartItems = [];
+      state.totalQuantity = 0;
+    },
+  },
+});
+
+export const cartActions = cartSlice.actions;
+export default cartSlice.reducer;
