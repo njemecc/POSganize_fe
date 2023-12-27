37f3905b443fdbd58b3f750c1c9c87ea2c589224
brani <branislannjemec@gmail.com>
2023-12-27 15:13:11 +0100

    add user image

--- src/pages/UserDetails.jsx
+++ src/pages/UserDetails.jsx
@@ -7,15 +7,40 @@ import UserDetailsHeaderv2 from "../features/users/UserDetailsHeaderv2";
 import Heading from "../ui/Heading";
 import Spinner from "../ui/Spinner";
 import { ADMIN } from "../utils/roles";
+import { useState, useEffect } from "react";
 
 const UserDetails = () => {
   const { role, isLoading } = useUser();
+
+  const [image, setImage] = useState(localStorage.getItem("user").image);
+
+  useEffect(() => {
+    setImage(JSON.parse(localStorage.getItem("user")).image);
+  }, [localStorage.getItem("user")]);
 
   isLoading && <Spinner />;
 
   return (
     <>
       {/* <UserDetailsHeader /> */}
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          width: "20rem",
+          margin: "auto",
+        }}
+      >
+        <img
+          style={{
+            maxWidth: "100%",
+            maxHeight: "100%",
+            borderRadius: "15px",
+            objectFit: "contain",
+          }}
+          src={`data:image/png;base64,${image}`}
+        ></img>
+      </div>
       <UserDetailsHeaderv2 />
 
       <Heading as="h2">
