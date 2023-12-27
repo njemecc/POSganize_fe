6003d7cc642cbcd59872ea6d479315e67a174630
brani <branislannjemec@gmail.com>
2023-12-27 19:29:57 +0100

    add allCartItems on the page

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import AllCartItems from "../features/cart/allCartItems";
 import Heading from "../ui/Heading";
 
 const CartPage = () => {
   return (
     <>
       <Heading>Your cart items</Heading>
+      <AllCartItems />
     </>
   );
 };
