04d25ccce79650be300127733c431fbfd4a18d29
brani <branislannjemec@gmail.com>
2023-12-27 15:59:22 +0100

    create CartPage

---
+++ src/pages/CartPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Heading from "../ui/Heading";
+
+const CartPage = () => {
+  return (
+    <>
+      <Heading>Your cart items</Heading>
+    </>
+  );
+};
+
+export default CartPage;
