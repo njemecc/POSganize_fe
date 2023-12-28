ea77e065766e2363b6ef50d27acbb2661cecb3dc
brani <branislannjemec@gmail.com>
2023-12-28 23:20:15 +0100

    render subscribe button based on role

--- src/features/trainings/TrainingCardv2.jsx
+++ src/features/trainings/TrainingCardv2.jsx
@@ -8,9 +8,12 @@ import { Card } from "@mui/material";
 import { cartActions } from "../../store/slices/cartSlice";
 import { useDispatch } from "react-redux";
 import { toast } from "react-hot-toast";
+import { USER } from "../../utils/roles";
+import { useUser } from "../authentication/useUser";
 
 const TrainingCardv2 = ({ training }) => {
   const { id, name, image, price, description } = training;
+  const { role } = useUser();
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
@@ -31,7 +34,9 @@ const TrainingCardv2 = ({ training }) => {
         <h2 class="card-title">{name}</h2>
         <p className="card-body">{`${description.substring(0, 150)}...`}</p>
         <p class="card-body">{price}$</p>
-        <Button onClick={addTrainingToCartHandler}>Subscribe</Button>
+        {role === USER && (
+          <Button onClick={addTrainingToCartHandler}>Subscribe</Button>
+        )}
       </div>
     </Card>
   );
