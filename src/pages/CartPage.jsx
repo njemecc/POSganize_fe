b546eb74aae6350e769367106790795154dc346e
brani <branislannjemec@gmail.com>
2024-01-03 23:11:04 +0100

    add button if cart is empty to redirect to trainings

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -3,16 +3,27 @@ import AllCartItems from "../features/cart/AllCartItems";
 import Heading from "../ui/Heading";
 //redux
 import { useSelector } from "react-redux";
+import Button from "../ui/Button";
+import { useNavigate } from "react-router-dom";
 
 const CartPage = () => {
   const cartItems = useSelector((state) => state.cart.cartItems);
+  const navigate = useNavigate();
   return cartItems.length > 0 ? (
     <>
       <Heading>New membership trainings</Heading>
       <AllCartItems cartItems={cartItems} />
     </>
   ) : (
-    <Heading>Membership package empty</Heading>
+    <div>
+      <Heading>Membership package empty</Heading>
+      <Button
+        onClick={() => navigate("/trainings")}
+        style={{ marginTop: "1rem" }}
+      >
+        check trainings
+      </Button>
+    </div>
   );
 };
 
